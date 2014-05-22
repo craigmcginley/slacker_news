daae92a16d42f5bac1397b931f2640513795045d
craigmcginley <craigmcginley@gmail.com>
2014-05-22 19:15:38 -0400

    *Index page now displays dynamically from csv file *Added set up for post page (new.erb)

--- server.rb
+++ server.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
-require 'uri'
+require 'csv'
 
+def get_articles
+  articles = []
+  CSV.foreach('articles.csv', headers: true, header_converters: :symbol) do |row|
+    articles << row.to_hash
+  end
+  articles
+end
 
 get '/' do
+  @articles = get_articles
 
   erb :index
+end
+
+get '/new' do
+
+  erb :new
+end
+
+post '/new' do
+
+
 end
